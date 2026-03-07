c3972304be58f23403781e13a0ba957a5d9309bd
Doogie Min <trollbellator@gmail.com>
2026-03-07 10:34:27 +0900

    tls: Add certificate compression support (RFC 8879) (#42690)
    Add TLS certificate compression with brotli and zlib algorithms.
    This reduces TLS handshake size, especially beneficial for QUIC where
    the ServerHello needs to fit in the initial
    response.
    
    The existing QUIC-only certificate compression implementation has been
    refactored to be shared between QUIC and TCP TLS. The QUIC wrapper now
    delegates to the common TLS implementation for
    backward compatibility.
    
    Fixes #42682
    
    Signed-off-by: Doogie Min <doogie.min@sendbird.com>

--- source/common/quic/envoy_quic_proof_source.cc
+++ source/common/quic/envoy_quic_proof_source.cc
@@ -7,10 +7,8 @@
 
 #include "envoy/ssl/tls_certificate_config.h"
 
-#include "source/common/quic/cert_compression.h"
 #include "source/common/quic/envoy_quic_utils.h"
 #include "source/common/quic/quic_io_handle_wrapper.h"
-#include "source/common/runtime/runtime_features.h"
 #include "source/common/stream_info/stream_info_impl.h"
 #include "source/common/tls/context_config_impl.h"
 #include "source/common/network/utility.h"
@@ -134,7 +132,7 @@ void EnvoyQuicProofSource::updateFilterChainManager(
 }
 
 void EnvoyQuicProofSource::OnNewSslCtx(SSL_CTX* ssl_ctx) {
-  CertCompression::registerSslContext(ssl_ctx);
+  registerCertCompression(ssl_ctx);
 
   // Try to set up keylog callback for QUIC SSL contexts
   setupQuicKeylogCallback(ssl_ctx);
